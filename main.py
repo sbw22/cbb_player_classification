4ef9ebb401ce4e7925617257a96a212d31b50318
Spencer Weishaar <sbweishaar@icloud.com>
2025-08-12 23:41:27 -0500

    Added csv of player data from 2024-25 season. Should have all available data for all players from website.

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+
+def main():
+
+    # TO DO:
+    # 1. Import data
+    # 2. Figure out how to classify data
+    # 2A - We can use clustering to classify players based on their stats. We can use K-Means or Hierarchical Clustering. We can also chart a 2d graph with basic stats. 
+    # 3. Save data somewhere
+
+    
+    pass
+
+if __name__ == "__main__":
+    main()
